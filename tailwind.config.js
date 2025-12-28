5cadb2e4e46805428f5997cc7ff7e722943b50ed
Ubuntu <ubuntu@ip-172-31-21-236.eu-west-1.compute.internal>
2025-12-28 00:30:53 +0000

    Feat: Tailwind working with basic index

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./suckerpunch/templates/**/*.html",
+    "./suckerpunch/static/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
